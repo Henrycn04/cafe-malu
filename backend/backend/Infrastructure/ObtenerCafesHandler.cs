e6bbc17900d22b0891a00fdba386a3296d3d9ca6
Bianchini <felipe.bianchini@ucr.ac.cr>
2025-01-16 16:45:25 -0600

    Get endpoints para obtener clientes y cafes

---
+++ backend/backend/Infrastructure/ObtenerCafesHandler.cs
@@ -0,0 +1,56 @@
+﻿using backend.Domain;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace backend.Infrastructure
+{
+    public interface IObtenerCafe
+    {
+        ListaCafesModel ObtenerCafes();
+    }
+    public class ObtenerCafesHandler : IObtenerCafe
+    {
+        private SqlConnection _connection;
+        private string _routeConnection;
+
+        public ObtenerCafesHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _routeConnection = builder.Configuration.GetConnectionString("CafeMaluContext");
+            _connection = new SqlConnection(_routeConnection);
+        }
+
+        public ListaCafesModel ObtenerCafes()
+        {
+            List<CafeModel> listaCafes = new List<CafeModel>();
+            string query = @"SELECT * FROM Cafe";
+
+            try
+            {
+                _connection.Open();
+                using (SqlCommand commandForQuery = new SqlCommand(query, _connection))
+                using (SqlDataReader reader = commandForQuery.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var cafe = new CafeModel
+                        {
+                            ID = reader.GetInt32("ID"),
+                            Nombre = reader.GetString("Nombre"),
+                            Peso = (double)reader.GetDecimal("Peso"),
+                            Especialidad = reader.GetBoolean("Especialidad"),
+                            PrecioUnitario = (double)reader.GetDecimal("PrecioUnitario")
+                        };
+                        listaCafes.Add(cafe);
+                    }
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return new ListaCafesModel { Cafes = listaCafes };
+        }
+    }
+}
---
+++ backend/backend/Infrastructure/ObtenerClientesHandler.cs
@@ -0,0 +1,56 @@
+﻿using backend.Domain;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace backend.Infrastructure
+{
+    public interface IObtenerClientes
+    {
+        ListaClientesModel ObtenerClientes();
+    }
+    public class ObtenerClientesHandler : IObtenerClientes
+    {
+        private SqlConnection _connection;
+        private string _routeConnection;
+
+        public ObtenerClientesHandler()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _routeConnection = builder.Configuration.GetConnectionString("CafeMaluContext");
+            _connection = new SqlConnection(_routeConnection);
+        }
+
+        public ListaClientesModel ObtenerClientes()
+        {
+            List<ClienteModel> listaClientes = new List<ClienteModel>();
+            string query = @"SELECT * FROM Cliente";
+
+            try
+            {
+                _connection.Open();
+                using (SqlCommand commandForQuery = new SqlCommand(query, _connection))
+                using (SqlDataReader reader = commandForQuery.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var cliente = new ClienteModel
+                        {
+                            ID = reader.GetInt32("ID"),
+                            Nombre = reader.GetString("Nombre"),
+                            ClienteBusqueda = reader.GetString("ClienteBusqueda"),
+                            Frecuencia = (int)reader.GetInt32("Frecuencia"),
+                            NumeroTelefonico = reader.GetString("NumeroTelefonico")
+                        };
+                        listaClientes.Add(cliente);
+                    }
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return new ListaClientesModel { Clientes = listaClientes };
+        }
+    }
+}
